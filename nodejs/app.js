3433e14f73b8058d411722aec2d87c562b33e4c2
Andry Mironov <andry.mironov94@gmail.com>
2019-01-17 10:46:14 +0200

    1.4 Piping html

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 
 var server = http.createServer((req, res) => {
   console.log("Request was made: ", req.url);
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  const myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf-8");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  const myReadStream = fs.createReadStream(__dirname + "/index.html", "utf-8");
   myReadStream.pipe(res);
 });
 server.listen(3000, "127.0.0.1");
