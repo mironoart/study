285c13403bd8703f710dc3ae146e89d2aa1d1ef0
Andry Mironov <andry.mironov94@gmail.com>
2019-01-18 14:40:22 +0200

    1.6 Express

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,6 +1,39 @@
-const http = require("http");
-const fs = require("fs");
+/* const http = require("http");
+const fs = require("fs"); */
 
+const express = require("express");
+
+// in express it evaluates right routes under the hood:
+const app = express();
+app.get("/", (req, res) => {
+  res.send("This is the message");
+});
+app.get("/contact", (req, res) => {
+  res.send("This is the contact");
+});
+
+app.listen(3000);
+
+/* 
+var server = http.createServer((req, res) => {
+  console.log("Request was made: ", req.url);
+  // if use " || /" then every request will return home page too!
+  // and be careful its async functions so no garantee what res will return first
+
+  if (req.url === "/home" || "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/index.html").pipe(res);
+  }
+  if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/contact.html").pipe(res);
+  }
+});
+server.listen(3000, "127.0.0.1");
+console.log("Litening to poort 3000");
+
+ */
+/* 
 var server = http.createServer((req, res) => {
   console.log("Request was made: ", req.url);
   res.writeHead(200, { "Content-Type": "application/json" });
@@ -13,7 +46,7 @@ var server = http.createServer((req, res) => {
 });
 server.listen(3000, "127.0.0.1");
 console.log("Litening to poort 3000");
-
+ */
 /* 
 
 //every time when new chunk recieved it fires this function ( every 65486 bytes)
