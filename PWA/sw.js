8f240480d373760f6287c60bbd0f37e7a0f11863
Andry Mironov <andry.mironov94@gmail.com>
2019-06-17 20:46:01 +0300

    Dynamic fallback page
    if some of images, for example, wont be available in cache fallback
    page will be returned, so we check if index.html is in cache, if not
    then we return fallback page

--- PWA/sw.js
+++ PWA/sw.js
@@ -55,6 +55,10 @@ self.addEventListener('fetch', evt => {
                })
             )
          })
-         .catch(() => caches.match('/pages/fallback.html'))
+         .catch(() => {
+            if (evt.request.url.indexOf('.html') > -1) {
+               return caches.match('/pages/fallback.html')
+            }
+         })
    )
 })
