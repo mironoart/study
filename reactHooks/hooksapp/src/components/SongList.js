8d6623c10ac1a11f70ea1cf83bc4cd262c2c918c
Andry Mironov <andry.mironov94@gmail.com>
2019-06-20 09:38:37 +0300

    setup hooks project

---
+++ reactHooks/hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SongList = () => {
+   return (
+      <div className="song-list">
+         <ul>
+            <li>Song Number One</li>
+            <li> Song Number Two</li>
+         </ul>
+      </div>
+   )
+}
+
+export default SongList
