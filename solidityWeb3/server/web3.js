f5e211673bbc71c54f87da557a7a0649cca7b024
Andry Mironov <andry.mironov94@gmail.com>
2019-06-29 10:43:19 +0300

    Events

--- solidityWeb3/server/web3.js
+++ solidityWeb3/server/web3.js
@@ -1,5 +1,5 @@
-const Web3 = require('web3');
+const Web3 = require('web3')
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/d324ab6757d84beeae6d7471e3184d55'));
+const web3 = new Web3('https://mainnet.infura.io/v3/d324ab6757d84beeae6d7471e3184d55')
 
-module.exports = web3;
+module.exports = web3
