5b5c2d11adf2107c1551705def1b07fa752dee6a
Andry Mironov <andry.mironov94@gmail.com>
2019-07-01 22:36:26 +0300

    Creating Contexts with Functional Components

--- reactHooks/contextapp/src/App.js
+++ reactHooks/contextapp/src/App.js
@@ -4,19 +4,22 @@ import Navbar from './components/Navbar'
 import ThemeContextProvider from './contexts/ThemeContext'
 import ThemeToggle from './components/ThemeToggle'
 import AuthContextProvider from './contexts/AuthContext'
+import BookContextProvider from './contexts/BookContext'
 
 function App() {
-   return (
-      <div className="App">
-         <ThemeContextProvider>
-            <AuthContextProvider>
-               <Navbar />
-               <BookList />
-               <ThemeToggle />
-            </AuthContextProvider>
-         </ThemeContextProvider>
-      </div>
-   )
+	return (
+		<div className="App">
+			<ThemeContextProvider>
+				<AuthContextProvider>
+					<Navbar />
+					<BookContextProvider>
+						<BookList />
+					</BookContextProvider>
+					<ThemeToggle />
+				</AuthContextProvider>
+			</ThemeContextProvider>
+		</div>
+	)
 }
 
 export default App
