d7cc9f1d44b3c5170a147ec88fb0587ed08a3bee
Andry Mironov <andrew.mironov@code-care.pro>
2019-07-02 13:49:05 +0300

    smartContractClass

--- solidityWeb3/server/index.js
+++ solidityWeb3/server/index.js
@@ -1,29 +1,29 @@
 require('dotenv').config();
-
 const express = require('express');
-//const bodyParser = require('body-parser');
+const smartContract = require('./smartContractAPI/smartContractApi');
+const abi = require('./abi.json');
 
 const app = express();
+
 /* 
+//const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/auth', auth);
+*/
 
-
-//from metamask
-account2 = '0x93551618d1a62b4a2abbe6f7486d259a5fbb136c';
-ownerAccount = process.env.OWNER_ACCOUNT;
-const privateKey1 = Buffer.from(process.env.OWNER_PRIVATE_KEY, 'hex');
-
-
- */
-
-const smartContract = require('./smartContractAPI/smartContractApi');
+const ownerAddress = process.env.OWNER_ADDRESS;
+const ownerPrivateKey = process.env.OWNER_PRIVATE_KEY;
+const smartContractAddress = process.env.SMART_CONTRACT_ADDRESS;
+const account2 = '0x93551618d1a62b4a2abbe6f7486d259a5fbb136c';
+const account3 = '0xAF06c8AD44E4019Fc2639fb35ACD3A7260122263';
+const contract = new smartContract(smartContractAddress, abi, ownerAddress, ownerPrivateKey);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, async () => {
 	console.log(`Server running on port ${port}`);
-	const temp = await smartContract.balanceOfAccount('0xbB8F59b1B9784De0DBa59B8fC42B3c7e8085c84C');
+	const temp = await contract.transfer(account2, account3, 61);
+
 	console.log(temp.toString());
 });
--- solidityWeb3/server/smartContractAPI/smartContractApi.js
+++ solidityWeb3/server/smartContractAPI/smartContractApi.js
@@ -1,65 +1,70 @@
 const EthereumTx = require('ethereumjs-tx').Transaction;
 const web3 = require('./web3');
-const abi = require('./ABI.json');
-const contract = new web3.eth.Contract(abi, process.env.SMART_CONTRACT_ADDRESS);
 
 class SmartContractApi {
-	static async balanceOfAccount(account) {
-		return await contract.methods.balanceOf(account).call();
-	}
-	static async getNameOfContract() {
-		return await contract.methods.name.call();
-	}
-	static async getOwnerOfToken(tokenId) {
-		return await contract.methods.ownerOf(tokenId).call();
-	}
-	static async getSymbolOfToken() {
-		return await contract.methods.symbol.call();
-	}
-	static async getTokenByIndex(index) {
-		return await contract.methods.tokenByIndex(index).call();
-	}
-	static async getTotalSupply() {
-		return await contract.methods.totalSupply().call();
+	constructor(smartContractAddress, abi, ownerAddress, ownerPrivateKey) {
+		this.ownerAddress = ownerAddress;
+		this.privateKey = Buffer.from(ownerPrivateKey, 'hex');
+		this.smartContractAddress = smartContractAddress;
+		this.contract = new web3.eth.Contract(abi, smartContractAddress);
 	}
 
-	static async mint(toAddress, tokenId) {
-		const txCount = await isError(web3.eth.getTransactionCount(ownerAccount));
+	balanceOfAccount(account) {
+		return this.contract.methods.balanceOf(account).call();
+	}
+	getNameOfContract() {
+		return this.contract.methods.name.call();
+	}
+	getOwnerOfToken(tokenId) {
+		return this.contract.methods.ownerOf(tokenId).call();
+	}
+	getSymbolOfToken() {
+		return this.contract.methods.symbol.call();
+	}
+	getTokenByIndex(index) {
+		return this.contract.methods.tokenByIndex(index).call();
+	}
+	getTotalSupply() {
+		return this.contract.methods.totalSupply().call();
+	}
+
+	async mint(toAddress, tokenId) {
+		const txCount = await web3.eth.getTransactionCount(this.ownerAddress);
 
 		const txObject = {
 			nonce: web3.utils.toHex(txCount),
 			gasLimit: web3.utils.toHex(800000),
 			gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
-			to: process.env.SMART_CONTRACT_ADDRESS,
-			data: contract.methods.mint(toAddress, tokenId).encodeABI()
+			to: this.smartContractAddress,
+			data: this.contract.methods.mint(toAddress, tokenId).encodeABI()
 		};
 
 		const tx = new EthereumTx(txObject, { chain: 'rinkeby' });
-		tx.sign(process.env.OWNER_PRIVATE_KEY);
+		tx.sign(this.privateKey);
 
 		const serializedTransaction = tx.serialize();
-		raw = '0x' + serializedTransaction.toString('hex');
+		const raw = '0x' + serializedTransaction.toString('hex');
 
 		txHash = await web3.eth.sendSignedTransaction(raw);
 		console.log(txHash);
 	}
 
-	static async transfer(fromAddress, toAddress, tokenId) {
-		const txCount = await isError(web3.eth.getTransactionCount(ownerAccount));
+	async transfer(fromAddress, toAddress, tokenId) {
+		const txCount = await web3.eth.getTransactionCount(this.ownerAddress);
 
 		const txObject = {
 			nonce: web3.utils.toHex(txCount),
 			gasLimit: web3.utils.toHex(800000),
 			gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
-			to: process.env.SMART_CONTRACT_ADDRESS,
-			data: contract.methods.safeTransferFrom(fromAddress, toAddress, tokenId).encodeABI()
+			to: this.smartContractAddress,
+			data: this.contract.methods.safeTransferFrom(fromAddress, toAddress, tokenId).encodeABI()
 		};
 
 		const tx = new EthereumTx(txObject, { chain: 'rinkeby' });
-		tx.sign(process.env.OWNER_PRIVATE_KEY);
+		tx.sign(this.privateKey);
 
 		const serializedTransaction = tx.serialize();
-		raw = '0x' + serializedTransaction.toString('hex');
+		const raw = '0x' + serializedTransaction.toString('hex');
 
 		txHash = await web3.eth.sendSignedTransaction(raw);
 		console.log(txHash);
